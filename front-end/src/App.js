f1c0b225f91543b339c07aa471a2672475400ccc
DashaTsybulkina <cybulkinadara@gmail.com>
2024-02-08 16:44:10 +0300

    changed design

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import {Form} from "./components/form/Form";
 import "./App.css";
 import DataList from "./components/datalist/DataList";
